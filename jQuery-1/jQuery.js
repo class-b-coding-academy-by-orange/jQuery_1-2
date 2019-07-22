1dafc391d3c39c7bfa8b555dac0ce6ad4da88b44
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-07-22 11:19:13 +0300

    upload till 13 question

---
+++ jQuery-1/jQuery.js
@@ -0,0 +1,81 @@
+/*
+Q1:
+Check if jQuery is loaded
+*/
+
+
+/*
+Q2:
+If You would have very long website - How to help user with fast getting back on top :)
+*/
+
+
+/*
+Q3:
+If You would like to change sizes of Your div/button with on click :)
+*/
+
+
+/*
+Q4:
+Learn how to fadein your boxes
+*/
+
+
+/*
+Q5:
+User must first accept then he or she can signup
+*/
+
+
+/*
+Q6:
+Let them print
+*/
+
+
+/*
+Q7:
+Can username be too long? Yest it can - lets limit that
+*/
+
+
+/*
+Q8:
+Wanna make some words bolder, do it with jQuery
+*/
+
+
+/*
+Q9:
+Add new div to your website
+*/
+
+
+/*
+Q10:
+Select values from a JSON object using jQuery
+*/
+
+
+/*
+Q11:
+Add element within ul list
+*/
+
+
+/*
+Q12:
+Know what user puts into textbox
+*/
+
+
+/*
+Q13:
+Change input value
+*/
+
+
+
+// if you finish try a new thing by your self
+// or go and repeat the DOM exercises using jQuery 
